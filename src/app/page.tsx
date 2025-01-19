b13fe4488d14dcb6c1ff4c9264629d318d3747fd
= <=>
2025-01-19 12:43:58 +0100

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/modules/shadcn/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <span>Hello World</span>
+      <Button>BUTTON</Button>
+    </div>
+  );
+}
